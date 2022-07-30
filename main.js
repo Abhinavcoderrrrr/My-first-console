8605c058fcbe7a2a8c86d7cd7b564f35f7ff1e0e
Abhinavcoderrrrr <110269551+Abhinavcoderrrrr@users.noreply.github.com>
2022-07-29 18:44:58 -0700

    My first console
    my first console

---
+++ main.js
@@ -0,0 +1,28 @@
+var first_name = "Abhinav";
+var last_name = "jha";
+
+var a = 10;
+var b = 5;
+
+<p>
+    To set up visual studio code first you need to download this app
+    then open the app. Then go to extension and download live server,
+    extension make a folder with file explorer the open it in visual studio
+    code. Then make html,js and css file on vs code.
+
+    <P>
+        console is a part of coding. If you creating variables on vs code
+        so you can make console of any variables for example - console.log(any)
+        this is how to write console in coding. I know you are thinking how to see our console?
+        step 1- you need to live your project then right click any were in the web page.
+        step 2- click on inspect.
+        step 3- click on console.
+        and you can see your console.
+
+        
+    </P>
+</p>
+
+
+
+
